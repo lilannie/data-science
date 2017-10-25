aa0fd3e32bf0950b4ffabc248beda74dca5feecb
Annie Steenson <anniesteenso@gmail.com>
2017-10-25 14:38:32 -0700

    Setting up project2 boilerplate

---
+++ project2/src/MinHash.java
@@ -0,0 +1,19 @@
+public class MinHash {
+    MinHash(String folder, int numPermutations) {
+
+    }
+
+    String[] allDocs() {
+        return null;
+    }
+
+    double exactJaccard(String file1, String file2) {
+        return 0.0;
+    }
+
+    int minHashSig(String fileName) {
+        return 0;
+    }
+
+
+}
