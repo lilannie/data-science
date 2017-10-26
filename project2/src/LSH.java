80ed25161182216025a7259262822029e5d042bc
Annie Steenson <anniesteenso@gmail.com>
2017-10-25 18:30:05 -0700

    Commented out LSH methods

--- project2/src/LSH.java
+++ project2/src/LSH.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+
 public class LSH {
+    /**
+     *
+     * @param minHashMatrix the MinHash matrix of the document collection
+     * @param docNames an array of Strings consisting of names of documents/les in the
+     *                 document collection
+     * @param bands the number of bands to be used to perform locality sensitive hashing
+     */
     public LSH(int[][] minHashMatrix, String[] docNames, int bands) {
 
     }
 
-    public String[] nearDuplicatesOf(String docName) {
-        String[] duplicates = new String[0];
+    /**
+     *
+     * @param docName name of a document
+     * @return an array list of names of the near duplicate documents.
+     */
+    public ArrayList<String> nearDuplicatesOf(String docName) {
+        ArrayList<String> duplicates = new ArrayList<>();
 
         return duplicates;
     }
--- project2/src/NearDuplicates.java
+++ project2/src/NearDuplicates.java
@@ -1,6 +1,21 @@
-public class NearDuplicates {
-    public String[] nearDuplicateDetector(String folder, int numPermutations, int s, String docName) {
+import java.util.ArrayList;
 
-        return new String[0];
+/**
+ *  class puts together MinHash and LSH to detect near duplicates in a document collection
+ */
+public class NearDuplicates {
+    /**
+     *
+     * @param folder name of the the folder containing documents
+     * @param numPermutations Number of Permutations to be used for MinHash
+     * @param s Similarity threshold
+     * @param docName Name of a document from the collection
+     * @return computes the list of documents that are more than s-similar to docName,
+     *          by calling the method nearDuplicates. Note that this list may contain some False Positives|
+     *          Documents that are less than s-similar to docName.
+     */
+    public ArrayList<String> nearDuplicateDetector(String folder, int numPermutations, int s, String docName) {
+        ArrayList<String> nearDuplicates = new ArrayList<>();
+        return nearDuplicates;
     }
 }
