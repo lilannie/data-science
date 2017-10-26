f15d7d9b75e153b342796923a379de6c0d1ad681
Annie Steenson <anniesteenso@gmail.com>
2017-10-25 18:24:14 -0700

    put the example .zip files in the gitignore

---
+++ project2/src/LSH.java
@@ -0,0 +1,11 @@
+public class LSH {
+    public LSH(int[][] minHashMatrix, String[] docNames, int bands) {
+
+    }
+
+    public String[] nearDuplicatesOf(String docName) {
+        String[] duplicates = new String[0];
+
+        return duplicates;
+    }
+}
---
+++ project2/src/NearDuplicates.java
@@ -0,0 +1,6 @@
+public class NearDuplicates {
+    public String[] nearDuplicateDetector(String folder, int numPermutations, int s, String docName) {
+
+        return new String[0];
+    }
+}
