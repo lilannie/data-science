f7738c01e8661d957f88fa86a6dd7c93f946a658
Annie Steenson <anniesteenso@gmail.com>
2017-11-14 20:24:51 -0600

    Added documentation to wiki ranker classes

---
+++ project3/src/MyWikiRanker.java
@@ -0,0 +1,11 @@
+/**
+ * Write a program named MyWikiRanker. For this, pick a set of words representing a topic of
+ * your choice. Choose an appropriate seed url and form a graph over 100 vertices. Compute the page
+ * rank each page in your graph, and output top page rank vertices.
+ */
+public class MyWikiRanker {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ project3/src/WikiTennisRanker.java
@@ -0,0 +1,14 @@
+/**
+ * Look at the attached graph named wikiTennis.txt. Run your page rank algorithm on this
+ * graph. Compute the page ranks with c = 0:01 as approximation factor. Output top 10 pages
+ * with highest page rank, highest in-degree and highest out-degree. Compute the following
+ * sets: Top 100 pages as per page rank, top 100 pages as per in-degree and top 100 pages as
+ * per out degree. For each pair of the sets, compute Jaccard Similarity. Repeat the same with
+ * c = 0:005 as approximation factor.
+ */
+public class WikiTennisRanker {
+
+    public static void main(String[] args) {
+
+    }
+}
