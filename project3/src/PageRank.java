86bde636fd53b0c96002aa6c37ea662aa1aa0b3a
Annie Steenson <anniesteenso@gmail.com>
2017-11-14 22:26:11 -0600

    Added code to PageRank that constructs the adjacency list to represent the graph

--- project3/src/PageRank.java
+++ project3/src/PageRank.java
@@ -1,28 +1,77 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class PageRank {
-    String fileName = "";
+    final double basis = 0.85;
+
     double approximation = 0.0;
+    int numVertexes = -1;
     int numEdges = 0;
 
     HashMap<String, Integer> vertexIndices = new HashMap<>();
-    HashMap<String, Integer> outDegree = new HashMap<>();
     HashMap<String, Integer> inDegree = new HashMap<>();
+    HashMap<String, HashSet<String>> graph = new HashMap<>();
+
     /**
-     *
      * @param fileName Name of a file that contains the edges of the graph. You may assume that the first line of
      *                 this graph lists the number of vertices, and every line (except first) lists one edge. You may
      *                 assume that each vertex is represented as string, and every edge of the graph appears exactly
      *                 once and the graph has no self loops.
+     *
      * @param approximation approximation parameter for pagerank.
      */
     public PageRank(String fileName, double approximation) {
-        this.fileName = fileName;
         this.approximation = approximation;
+
+        initializeGraph(fileName);
+        computePageRankVector();
+    }
+
+    private void initializeGraph(String fileName) {
+        BufferedReader br = null;
+        FileReader fr = null;
+        try {
+            fr = new FileReader(fileName);
+            br = new BufferedReader(fr);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                if (this.numVertexes < 0) {
+                    this.numVertexes = Integer.parseInt(line);
+                    continue;
+                }
+
+                String[] vertices = line.split(" ");
+                addEdge(vertices[0], vertices[1]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addEdge(String from, String to) {
+        if (!graph.containsKey(from)) {
+            graph.put(from, new HashSet<>());
+        }
+
+        graph.get(from).add(to);
+
+        int inDegreeCount = 1;
+        if (inDegree.containsKey(to)) {
+            inDegreeCount += inDegree.get(to);
+        }
+        inDegree.put(to, inDegreeCount);
+    }
+
+    private void computePageRankVector() {
+
     }
 
     /**
-     *
      * @param vertex name of vertex of the graph
      * @return its page rank
      */
@@ -32,7 +81,6 @@ public class PageRank {
     }
 
     /**
-     *
      * @param vertex name of vertex of the graph
      * @return its out degree
      */
@@ -42,7 +90,6 @@ public class PageRank {
     }
 
     /**
-     *
      * @param vertex name of vertex of the graph
      * @return its in degree
      */
@@ -52,7 +99,6 @@ public class PageRank {
     }
 
     /**
-     *
      * @return number of edges of the graph.
      */
     public int numEdges() {
@@ -60,7 +106,6 @@ public class PageRank {
     }
 
     /**
-     *
      * @param k parameter
      * @return an array (of strings) of pages with top k page ranks.
      */
@@ -70,7 +115,6 @@ public class PageRank {
     }
 
     /**
-     *
      * @param k parameter
      * @return returns an array (of strings) of pages with top k in degree.
      */
@@ -80,12 +124,15 @@ public class PageRank {
     }
 
     /**
-     *
      * @param k parameter
      * @return returns an array (of strings) of pages with top k out degree.
      */
     public String[] topKOutDegree(int k) {
 
         return new String[0];
+    }
+
+    public static void main(String[] args) {
+        PageRank pr = new PageRank("WikiTennisGraph.txt", 0.0);
     }
 }
